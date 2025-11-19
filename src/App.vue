2702eb279b03cfdeed0e45e4a285389c4fbf64f1
00parkyh <00parkyh@naver.com>
2025-11-19 16:57:22 +0900

    homework

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+// import Practice1 from './components/Practice1.vue';
+import Practice1_composition from './components/Practice1_composition.vue';
+</script>
+<template>
+  <!-- <Practice1 /> -->
+  <Practice1_composition />
+</template>
